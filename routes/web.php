877b8a73b75956d2c9b7c426abd96c1ea1ae9bce
Yoga Hanggara <yoga@javan.co.id>
2018-03-28 18:29:25 +0700

    pertemuan kedua

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('violations', 'ViolationController');
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
